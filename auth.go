41d2d4ef7dde0b4af535021e56408b0d61c43d89
Aymeric <aymeric@firebolt.io>
2022-11-15 16:18:27 +0100

    Add support for service account

--- auth.go
+++ auth.go
@@ -3,6 +3,8 @@ package fireboltgosdk
 import (
 	"context"
 	"encoding/json"
+	"net/url"
+	"strings"
 )
 
 type AuthenticationResponse struct {
@@ -15,16 +17,21 @@ type AuthenticationResponse struct {
 
 // Authenticate sends an authentication request, and returns a newly constructed client object
 func Authenticate(username, password, apiEndpoint string) (*Client, error) {
-	infolog.Printf("Start authentication into '%s' using '%s'", apiEndpoint, LoginUrl)
-
-	values := map[string]string{"username": username, "password": password}
-	jsonData, err := json.Marshal(values)
-	if err != nil {
-		return nil, ConstructNestedError("error during json marshalling", err)
+	var loginUrl string
+	var contentType string
+	var body string
+	var err error
+	if strings.Contains(username, "@") {
+		loginUrl, contentType, body, err = prepareUsernamePasswordLogin(username, password)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		loginUrl, contentType, body = prepareServiceAccountLogin(username, password)
 	}
-
+	infolog.Printf("Start authentication into '%s' using '%s'", apiEndpoint, loginUrl)
 	userAgent := ConstructUserAgentString()
-	resp, err := request(context.TODO(), "", "POST", apiEndpoint+LoginUrl, userAgent, nil, string(jsonData))
+	resp, err := request(context.TODO(), "", "POST", apiEndpoint+loginUrl, userAgent, nil, body, contentType)
 	if err != nil {
 		return nil, ConstructNestedError("authentication request failed", err)
 	}
@@ -36,4 +43,28 @@ func Authenticate(username, password, apiEndpoint string) (*Client, error) {
 
 	infolog.Printf("Authentication was successful")
 	return &Client{AccessToken: authResp.AccessToken, ApiEndpoint: apiEndpoint, UserAgent: userAgent}, nil
+}
+
+func prepareUsernamePasswordLogin(username string, password string) (string, string, string, error) {
+	var authUrl = UsernamePasswordURLSuffix
+	//var values = map[string]string{"username": username, "password": password}
+	var contentType = "application/json"
+	values := map[string]string{"username": username, "password": password}
+	jsonData, err := json.Marshal(values)
+	if err != nil {
+		return "", "", "", ConstructNestedError("error during json marshalling", err)
+	} else {
+		return authUrl, contentType, string(jsonData), nil
+	}
+}
+
+func prepareServiceAccountLogin(username, password string) (string, string, string) {
+	var authUrl = ServiceAccountLoginURLSuffix
+	form := url.Values{}
+	form.Add("client_id", username)
+	form.Add("client_secret", password)
+	form.Add("grant_type", "client_credentials")
+	var contentType = "application/x-www-form-urlencoded"
+	var body = form.Encode()
+	return authUrl, contentType, body
 }
--- client.go
+++ client.go
@@ -26,7 +26,7 @@ func (c *Client) GetAccountIdByName(ctx context.Context, accountName string) (st
 
 	params := map[string]string{"account_name": accountName}
 
-	response, err := request(ctx, c.AccessToken, "GET", c.ApiEndpoint+AccountIdByNameURL, c.UserAgent, params, "")
+	response, err := request(ctx, c.AccessToken, "GET", c.ApiEndpoint+AccountIdByNameURL, c.UserAgent, params, "", "")
 	if err != nil {
 		return "", ConstructNestedError("error during getting account id by name request", err)
 	}
@@ -51,7 +51,7 @@ func (c *Client) GetEngineIdByName(ctx context.Context, engineName string, accou
 	}
 
 	params := map[string]string{"engine_name": engineName}
-	response, err := request(ctx, c.AccessToken, "GET", fmt.Sprintf(c.ApiEndpoint+EngineIdByNameURL, accountId), c.UserAgent, params, "")
+	response, err := request(ctx, c.AccessToken, "GET", fmt.Sprintf(c.ApiEndpoint+EngineIdByNameURL, accountId), c.UserAgent, params, "", "")
 	if err != nil {
 		return "", ConstructNestedError("error during getting engine id by name request", err)
 	}
@@ -74,7 +74,7 @@ func (c *Client) GetEngineUrlById(ctx context.Context, engineId string, accountI
 		Engine EngineResponse `json:"engine"`
 	}
 
-	response, err := request(ctx, c.AccessToken, "GET", fmt.Sprintf(c.ApiEndpoint+EngineByIdURL, accountId, engineId), c.UserAgent, make(map[string]string), "")
+	response, err := request(ctx, c.AccessToken, "GET", fmt.Sprintf(c.ApiEndpoint+EngineByIdURL, accountId, engineId), c.UserAgent, make(map[string]string), "", "")
 
 	if err != nil {
 		return "", ConstructNestedError("error during getting engine url by id request", err)
@@ -97,7 +97,7 @@ func (c *Client) GetDefaultAccountId(ctx context.Context) (string, error) {
 		Account AccountResponse `json:"account"`
 	}
 
-	response, err := request(ctx, c.AccessToken, "GET", fmt.Sprintf(c.ApiEndpoint+DefaultAccountURL), c.UserAgent, make(map[string]string), "")
+	response, err := request(ctx, c.AccessToken, "GET", fmt.Sprintf(c.ApiEndpoint+DefaultAccountURL), c.UserAgent, make(map[string]string), "", "")
 	if err != nil {
 		return "", ConstructNestedError("error during getting default account id request", err)
 	}
@@ -136,7 +136,7 @@ func (c *Client) GetEngineUrlByDatabase(ctx context.Context, databaseName string
 	}
 
 	params := map[string]string{"database_name": databaseName}
-	response, err := request(ctx, c.AccessToken, "GET", fmt.Sprintf(c.ApiEndpoint+EngineUrlByDatabaseNameURL, accountId), c.UserAgent, params, "")
+	response, err := request(ctx, c.AccessToken, "GET", fmt.Sprintf(c.ApiEndpoint+EngineUrlByDatabaseNameURL, accountId), c.UserAgent, params, "", "")
 	if err != nil {
 		return "", ConstructNestedError("error during getting engine url by database request", err)
 	}
@@ -157,7 +157,7 @@ func (c *Client) Query(ctx context.Context, engineUrl, databaseName, query strin
 		params[setKey] = setValue
 	}
 
-	response, err := request(ctx, c.AccessToken, "POST", engineUrl, c.UserAgent, params, query)
+	response, err := request(ctx, c.AccessToken, "POST", engineUrl, c.UserAgent, params, query, "")
 	if err != nil {
 		return nil, ConstructNestedError("error during query request", err)
 	}
@@ -209,7 +209,7 @@ func checkErrorResponse(response []byte) error {
 // additionally it passes the parameters and a bodyStr as a payload
 // if accessToken is passed, it is used for authorization
 // returns response and an error
-func request(ctx context.Context, accessToken string, method string, url string, userAgent string, params map[string]string, bodyStr string) ([]byte, error) {
+func request(ctx context.Context, accessToken string, method string, url string, userAgent string, params map[string]string, bodyStr string, contentType string) ([]byte, error) {
 	req, _ := http.NewRequestWithContext(ctx, method, makeCanonicalUrl(url), strings.NewReader(bodyStr))
 
 	// adding sdk usage tracking
@@ -218,6 +218,9 @@ func request(ctx context.Context, accessToken string, method string, url string,
 	if len(accessToken) > 0 {
 		var bearer = "Bearer " + accessToken
 		req.Header.Add("Authorization", bearer)
+	}
+	if len(contentType) > 0 {
+		req.Header.Set("Content-Type", contentType)
 	}
 
 	q := req.URL.Query()
