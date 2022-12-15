6af4f684263d8d832ebd32373667a64491bc0dd9
Aymeric <aymeric@firebolt.io>
2022-12-15 17:05:16 +0000

    Enable service account auth test

--- auth_integration_test.go
+++ auth_integration_test.go
@@ -4,6 +4,7 @@
 package fireboltgosdk
 
 import (
+	"os"
 	"testing"
 )
 
@@ -28,31 +29,30 @@ func TestAuthEmptyCredential(t *testing.T) {
 	}
 }
 
-//
-//// TestAuthServiceAccount checks that authentication with service account works
-//func TestAuthServiceAccount(t *testing.T) {
-//	serviceAccountClientId = os.Getenv("SERVICE_ACCOUNT_CLIENT_ID")
-//	serviceAccountClientSecret = os.Getenv("SERVICE_ACCOUNT_CLIENT_SECRET")
-//	if len(serviceAccountClientId) == 0 {
-//		t.Errorf("Could not run the test because the client id is not set (environment var: SERVICE_ACCOUNT_CLIENT_ID")
-//	}
-//	if len(serviceAccountClientSecret) == 0 {
-//		t.Errorf("Could not run the test because the client secret is not set (environment var: SERVICE_ACCOUNT_CLIENT_SECRET")
-//	}
-//	client, err := Authenticate(serviceAccountClientId, serviceAccountClientSecret, GetHostNameURL())
-//	if err != nil {
-//		t.Errorf("Could not authenticate using service account")
-//	}
-//
-//	if serviceAccountClientId != client.Username {
-//		t.Errorf("The username set in the client doesn't match the service account client id")
-//	}
-//
-//	if serviceAccountClientSecret != client.Password {
-//		t.Errorf("The password set in the client doesn't match the service account client secret")
-//	}
-//	if len(getCachedAccessToken(serviceAccountClientId, GetHostNameURL())) == 0 {
-//		t.Errorf("Token is not set properly")
-//	}
-//
-//}
+// TestAuthServiceAccount checks that authentication with service account works
+func TestAuthServiceAccount(t *testing.T) {
+	serviceAccountClientId = os.Getenv("SERVICE_ACCOUNT_CLIENT_ID")
+	serviceAccountClientSecret = os.Getenv("SERVICE_ACCOUNT_CLIENT_SECRET")
+	if len(serviceAccountClientId) == 0 {
+		t.Errorf("Could not run the test because the client id is not set (environment var: SERVICE_ACCOUNT_CLIENT_ID")
+	}
+	if len(serviceAccountClientSecret) == 0 {
+		t.Errorf("Could not run the test because the client secret is not set (environment var: SERVICE_ACCOUNT_CLIENT_SECRET")
+	}
+	client, err := Authenticate(serviceAccountClientId, serviceAccountClientSecret, GetHostNameURL())
+	if err != nil {
+		t.Errorf("Could not authenticate using service account")
+	}
+
+	if serviceAccountClientId != client.Username {
+		t.Errorf("The username set in the client doesn't match the service account client id")
+	}
+
+	if serviceAccountClientSecret != client.Password {
+		t.Errorf("The password set in the client doesn't match the service account client secret")
+	}
+	if len(getCachedAccessToken(serviceAccountClientId, GetHostNameURL())) == 0 {
+		t.Errorf("Token is not set properly")
+	}
+
+}
