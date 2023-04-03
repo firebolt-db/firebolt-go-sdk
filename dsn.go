7a537ced4ec8f8b11fd569ff10ed7eb66ad0f3d4
Stepan Burlakov <stepansergeevitch@gmail.com>
2023-04-03 12:12:01 +0300

    update connection string parsing

--- dsn.go
+++ dsn.go
@@ -1,85 +1,59 @@
 package fireboltgosdk
 
 import (
+	"errors"
 	"fmt"
-	"strings"
+	"regexp"
 )
 
 type fireboltSettings struct {
-	username    string
-	password    string
-	database    string
-	engineName  string
-	accountName string
+	database     string
+	account_name string
+	engine       string
+	clientId     string
+	clientSecret string
 }
 
-// splitString split string into two string, when first split char is encountered
-// return two strings:
-//    - first string before the encountered character, with removed backtick escape
-//    - second string is remaining string, including the character on which it was split
-func splitString(str string, splitChars []uint8) (string, string) {
-	var res string
-	for i := 0; i < len(str); i++ {
-		if str[i] == '\\' {
-			res += string(str[i+1])
-			i += 1
-			continue
-		}
-		for _, v := range splitChars {
-			if str[i] == v {
-				return res, str[i:]
-			}
-		}
-		res += string(str[i])
-	}
-	return res, ""
-}
+const dsnPattern = `^firebolt://(?:/(?P<database>\w+))?(?:\?(?P<parameters>\w+\=\w+(?:\&\w+=\w+)*))?$`
+const paramsPattern = `(?P<key>\w+)=(?P<value>\w+)`
 
-// parseRemainingDSN checks whether dsn has an expected prefix, removes it, and return two strings:
-//    - before the stopChars
-//    - after the stopChars, including the char itself
-func parseRemainingDSN(dsn string, expectedPrefix string, stopChars []uint8) (string, string, error) {
-	if !strings.HasPrefix(dsn, expectedPrefix) {
-		return "", dsn, fmt.Errorf("expected prefix not found: %s", expectedPrefix)
-	}
-
-	dsn = dsn[len(expectedPrefix):]
-	res, dsn := splitString(dsn, stopChars)
-	return res, dsn, nil
-}
-
-// ParseDSNString parses a dsn in a format: firebolt://username:password@db_name[/engine_name][?account_name=organization]
+// ParseDSNString parses a dsn in a format: firebolt://[/database][?param=value[&param=value]]
+// Accepted parameters are: account_name, engine, client_id, client_secret
 // returns a settings object where all parsed values are populated
 // returns an error if required fields couldn't be parsed or if after parsing some characters were left unparsed
 func ParseDSNString(dsn string) (*fireboltSettings, error) {
 	var result fireboltSettings
+	dsnExpr := regexp.MustCompile(dsnPattern)
+	paramsExpr := regexp.MustCompile(paramsPattern)
 
-	var err error
+	dsnMatch := dsnExpr.FindStringSubmatch(dsn)
 
-	// parse username
-	result.username, dsn, err = parseRemainingDSN(dsn, "firebolt://", []uint8{':'})
-	if err != nil {
-		return nil, ConstructNestedError("error during username parsing", err)
+	if len(dsnMatch) == 0 {
+		return nil, errors.New("invalid connection string format")
 	}
 
-	// parse password
-	result.password, dsn, err = parseRemainingDSN(dsn, ":", []uint8{'@'})
-	if err != nil {
-		return nil, ConstructNestedError("error during password parsing", err)
+	// Set database if it's provided
+	if len(dsnMatch[1]) > 0 {
+		result.database = dsnMatch[1]
 	}
 
-	// parse database
-	result.database, dsn, err = parseRemainingDSN(dsn, "@", []uint8{'/', '?'})
-	if err != nil {
-		return nil, ConstructNestedError("error during database parsing", err)
-	}
-
-	// parse engine and account names
-	result.engineName, dsn, _ = parseRemainingDSN(dsn, "/", []uint8{'?'})
-	result.accountName, dsn, _ = parseRemainingDSN(dsn, "?account_name=", []uint8{})
-
-	if len(dsn) != 0 {
-		return nil, fmt.Errorf("unparsed characters were found: %s", dsn)
+	paramsStr := dsnMatch[2]
+	if len(paramsStr) > 0 {
+		paramsMatch := paramsExpr.FindAllStringSubmatch(paramsStr, -1)
+		for _, m := range paramsMatch {
+			switch m[1] {
+			case "account_name":
+				result.account_name = m[2]
+			case "engine":
+				result.engine = m[2]
+			case "client_id":
+				result.clientId = m[2]
+			case "client_secret":
+				result.clientSecret = m[2]
+			default:
+				return nil, fmt.Errorf("unknown parameter name %s", m[1])
+			}
+		}
 	}
 
 	return &result, nil
