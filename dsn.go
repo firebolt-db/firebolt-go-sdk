225557e621c21935baca1efb315f13243fe27710
Stepan Burlakov <stepansergeevitch@gmail.com>
2023-11-20 12:19:01 +0200

    rework dns parsing

--- dsn.go
+++ dsn.go
@@ -7,51 +7,89 @@ import (
 )
 
 type fireboltSettings struct {
-	username    string
-	password    string
-	database    string
-	engineName  string
-	accountName string
+	clientID     string
+	clientSecret string
+	database     string
+	engineName   string
+	accountName  string
+	newVersion   bool
 }
 
-const dsnPattern = `^firebolt://(?P<username>.*@?.*):(?P<password>.*)@(?P<database>\w+)(?:/(?P<engine>[^?]+))?(?:\?(?P<parameters>\w+\=[^=&]+(?:\&\w+=[^=&]+)*))?$`
+const dsnPattern = `^firebolt://(?:/(?P<database>\w+))?(?:\?(?P<parameters>\w+\=[^=&]+(?:\&\w+=[^=&]+)*))?$`
+const dsnPatternV0 = `^firebolt://(?P<username>.*@?.*):(?P<password>.*)@(?P<database>\w+)(?:/(?P<engine>[^?]+))?(?:\?(?P<parameters>\w+\=[^=&]+(?:\&\w+=[^=&]+)*))?$`
 const paramsPattern = `(?P<key>\w+)=(?P<value>[^=&]+)`
 
 // ParseDSNString parses a dsn in a format: firebolt://username:password@db_name[/engine_name][?account_name=organization]
 // returns a settings object where all parsed values are populated
 // returns an error if required fields couldn't be parsed or if after parsing some characters were left unparsed
 func ParseDSNString(dsn string) (*fireboltSettings, error) {
-	var result fireboltSettings
 	dsnExpr := regexp.MustCompile(dsnPattern)
-	paramsExpr := regexp.MustCompile(paramsPattern)
+	dsnExprV0 := regexp.MustCompile(dsnPatternV0)
 
 	infolog.Println("Parsing DSN")
-	dsnMatch := dsnExpr.FindStringSubmatch(dsn)
 
-	if len(dsnMatch) == 0 {
+	if dsnMatch := dsnExpr.FindStringSubmatch(dsn); len(dsnMatch) > 0 {
+		return makeSettings(dsnMatch)
+	} else if dsnMatch := dsnExprV0.FindStringSubmatch(dsn); len(dsnMatch) > 0 {
+		return makeSettingsV0(dsnMatch)
+	} else {
 		return nil, errors.New("invalid connection string format")
 	}
+}
 
-	result.username = dsnMatch[1]
-	result.password = dsnMatch[2]
+func makeSettings(dsnMatch []string) (*fireboltSettings, error) {
+	var result fireboltSettings
+	result.newVersion = true
+
+	// Set database if it's provided
+	if len(dsnMatch[1]) > 0 {
+		result.database = dsnMatch[1]
+	}
+	for _, m := range parseParams(dsnMatch[2]) {
+		switch m[1] {
+		case "account_name":
+			result.accountName = m[2]
+		case "engine":
+			result.engineName = m[2]
+		case "client_id":
+			result.clientID = m[2]
+		case "client_secret":
+			result.clientSecret = m[2]
+		default:
+			return nil, fmt.Errorf("unknown parameter name %s", m[1])
+		}
+	}
+	return &result, nil
+}
+
+func makeSettingsV0(dsnMatch []string) (*fireboltSettings, error) {
+	var result fireboltSettings
+	result.newVersion = false
+
+	result.clientID = dsnMatch[1]
+	result.clientSecret = dsnMatch[2]
 	result.database = dsnMatch[3]
 	if len(dsnMatch[4]) > 0 {
 		// engine name was provided
 		result.engineName = dsnMatch[4]
 	}
 
-	paramsStr := dsnMatch[5]
-	if len(paramsStr) > 0 {
-		paramsMatch := paramsExpr.FindAllStringSubmatch(paramsStr, -1)
-		for _, m := range paramsMatch {
-			switch m[1] {
-			case "account_name":
-				result.accountName = m[2]
-			default:
-				return nil, fmt.Errorf("unknown parameter name %s", m[1])
-			}
+	for _, m := range parseParams(dsnMatch[5]) {
+		switch m[1] {
+		case "account_name":
+			result.accountName = m[2]
+		default:
+			return nil, fmt.Errorf("unknown parameter name %s", m[1])
 		}
 	}
 
 	return &result, nil
+}
+
+func parseParams(paramsStr string) [][]string {
+	if len(paramsStr) == 0 {
+		return make([][]string, 0)
+	}
+	paramsExpr := regexp.MustCompile(paramsPattern)
+	return paramsExpr.FindAllStringSubmatch(paramsStr, -1)
 }
--- dsn_test.go
+++ dsn_test.go
@@ -1,10 +1,8 @@
 package fireboltgosdk
 
-func empty() {
+import "testing"
 
-}
-
-/*func runDSNTest(t *testing.T, input string, expectedSettings fireboltSettings) {
+func runDSNTest(t *testing.T, input string, expectedSettings fireboltSettings) {
 	settings, err := ParseDSNString(input)
 
 	if err != nil {
@@ -15,16 +13,24 @@ func empty() {
 		t.Errorf("for account_name got %s want %s", settings.accountName, expectedSettings.accountName)
 	}
 
-	if settings.engine != expectedSettings.engine {
-		t.Errorf("for engine got %s want %s", settings.engine, expectedSettings.engine)
+	if settings.engineName != expectedSettings.engineName {
+		t.Errorf("for engine got %s want %s", settings.engineName, expectedSettings.engineName)
 	}
 
-	if settings.clientId != expectedSettings.clientId {
-		t.Errorf("for client_id got %s want %s", settings.clientId, expectedSettings.clientId)
+	if settings.clientID != expectedSettings.clientID {
+		t.Errorf("for client_id got %s want %s", settings.clientID, expectedSettings.clientID)
 	}
 
 	if settings.clientSecret != expectedSettings.clientSecret {
 		t.Errorf("for client_secret got %s want %s", settings.clientSecret, expectedSettings.clientSecret)
+	}
+
+	if settings.database != expectedSettings.database {
+		t.Errorf("for database got %s want %s", settings.database, expectedSettings.database)
+	}
+
+	if settings.newVersion != expectedSettings.newVersion {
+		t.Errorf("for newVersion got %t want %t", settings.newVersion, expectedSettings.newVersion)
 	}
 }
 
@@ -36,19 +42,19 @@ func runDSNTestFail(t *testing.T, input string) {
 }
 
 func TestDSNHappyPath(t *testing.T) {
-	runDSNTest(t, "firebolt://", fireboltSettings{})
+	runDSNTest(t, "firebolt://", fireboltSettings{newVersion: true})
 
-	runDSNTest(t, "firebolt:///test_db", fireboltSettings{database: "test_db"})
+	runDSNTest(t, "firebolt:///test_db", fireboltSettings{database: "test_db", newVersion: true})
 
 	runDSNTest(t, "firebolt://?account_name=test_acc&engine=test_eng&client_id=test_cid&client_secret=test_cs",
-		fireboltSettings{accountName: "test_acc", engine: "test_eng", clientId: "test_cid", clientSecret: "test_cs"})
+		fireboltSettings{accountName: "test_acc", engineName: "test_eng", clientID: "test_cid", clientSecret: "test_cs", newVersion: true})
 
 	runDSNTest(t, "firebolt:///test_db?account_name=test_acc&engine=test_eng&client_id=test_cid&client_secret=test_cs",
-		fireboltSettings{database: "test_db", accountName: "test_acc", engine: "test_eng", clientId: "test_cid", clientSecret: "test_cs"})
+		fireboltSettings{database: "test_db", accountName: "test_acc", engineName: "test_eng", clientID: "test_cid", clientSecret: "test_cs", newVersion: true})
 
 	// special characters
 	runDSNTest(t, "firebolt:///test_db?account_name=test_acc&engine=test_eng&client_id=test_cid&client_secret=test_*-()@\\.",
-		fireboltSettings{database: "test_db", accountName: "test_acc", engine: "test_eng", clientId: "test_cid", clientSecret: "test_*-()@\\."})
+		fireboltSettings{database: "test_db", accountName: "test_acc", engineName: "test_eng", clientID: "test_cid", clientSecret: "test_*-()@\\.", newVersion: true})
 }
 
 // TestDSNFailed test different failure scenarios for ParseDSNString
@@ -59,4 +65,37 @@ func TestDSNFailed(t *testing.T) {
 	runDSNTestFail(t, "firebolt://?k=v")    // unknown parameter name
 	runDSNTestFail(t, "firebolt:///db?k=v") // unknown parameter name
 }
-*/
+
+func TestDSNV0HappyPath(t *testing.T) {
+	runDSNTest(t, "firebolt://user@firebolt.io:password@db_name",
+		fireboltSettings{clientID: "user@firebolt.io", clientSecret: "password", database: "db_name", newVersion: false})
+
+	runDSNTest(t, "firebolt://user@firebolt.io:password@db_name/engine_name",
+		fireboltSettings{clientID: "user@firebolt.io", clientSecret: "password", database: "db_name", engineName: "engine_name", newVersion: false})
+
+	runDSNTest(t, "firebolt://user@firebolt.io:password@db_name/engine_name",
+		fireboltSettings{clientID: "user@firebolt.io", clientSecret: "password", database: "db_name", engineName: "engine_name", newVersion: false})
+
+	runDSNTest(t, "firebolt://user@firebolt.io:password@db_name/engine_url.firebolt.io",
+		fireboltSettings{clientID: "user@firebolt.io", clientSecret: "password", database: "db_name", engineName: "engine_url.firebolt.io", newVersion: false})
+
+	runDSNTest(t, "firebolt://user@firebolt.io:password@db_name/https://engine_url.firebolt.io",
+		fireboltSettings{clientID: "user@firebolt.io", clientSecret: "password", database: "db_name", engineName: "https://engine_url.firebolt.io", newVersion: false})
+
+	runDSNTest(t, "firebolt://user@firebolt.io:password@db_name?account_name=firebolt_account",
+		fireboltSettings{clientID: "user@firebolt.io", clientSecret: "password", database: "db_name", accountName: "firebolt_account", newVersion: false})
+
+	runDSNTest(t, "firebolt://user@fire:bolt.io:passwo@rd@db_name?account_name=firebolt_account",
+		fireboltSettings{clientID: "user@fire:bolt.io", clientSecret: "passwo@rd", database: "db_name", accountName: "firebolt_account", newVersion: false})
+
+	runDSNTest(t, "firebolt://client_id:client_secret@db_name?account_name=firebolt_account",
+		fireboltSettings{clientID: "client_id", clientSecret: "client_secret", database: "db_name", accountName: "firebolt_account", newVersion: false})
+}
+
+// TestDSNFailed test different failure scenarios for ParseDSNString
+func TestDSNV0Failed(t *testing.T) {
+	runDSNTestFail(t, "")
+	runDSNTestFail(t, "firebolt://user:yury_db")
+	runDSNTestFail(t, "jdbc://user:yury_db@db_name")
+	runDSNTestFail(t, "firebolt://yury_db@dn_name?account_name=firebolt_account")
+}
