3fb237bd39ab29a4f5d1bb5b5f8572b9f7db4bd1
Stepan Burlakov <stepan.burlakov@firebolt.io>
2025-03-31 16:21:42 +0300

    add unauthorized error wrapper

--- errors/errors.go
+++ errors/errors.go
@@ -2,6 +2,7 @@ package errors
 
 var (
 	AuthenticationError         = ConstructNestedError("authentication error", nil)
+	UnauthorizedError           = ConstructNestedError("authorization error", nil)
 	QueryExecutionError         = ConstructNestedError("query execution error", nil)
 	SystemEngineResolutionError = ConstructNestedError("error getting system engine URL", nil)
 	DSNParseError               = ConstructNestedError("error parsing DSN", nil)
